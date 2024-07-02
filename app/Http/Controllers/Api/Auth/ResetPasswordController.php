d1118c7126525b0e6f3d3cfca1dcdaa0c9005ff0
osama ibrahim <smsmmarzok30@gmail.com>
2024-07-02 22:26:09 +0300

    note controller

---
+++ app/Http/Controllers/Api/Auth/ResetPasswordController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    public function __invoke(
+        Res
+    )
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
